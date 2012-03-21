ef609e652a29ad737044255aedbef79c1270677f
Nicolas <nicolas.papon@webflows.fr>
2012-03-21 22:42:37 +0100

    up ga

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,7 @@
 
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-26587970-1']);
+    _gaq.push(['_setDomainName', 'squid-corp.com']);
     _gaq.push(['_trackPageview']);
 
     (function() {
@@ -14,6 +15,7 @@
       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
+
 
     var ua = navigator.userAgent,
         click = (ua.match(/iPad/i)) ? "touchstart" : "click";
