8493ad8cb1f60323100f2633474129475ab6cf9e
David FRANCOIS <david.francois@webflows.fr>
2012-04-24 21:16:57 +0200

    Changement de serveur de deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :rails_env, ENV['rails_env'] || ENV['RAILS_ENV'] || default_env
 django = "django.webflows.fr"
 
 set :application, ""
-set :repository,  "https://github.com/itkin/squid.git"
+set :repository,  "https://github.com/davout/squid.git"
 
 set :scm, :git
 set :deploy_to, "~/www.squid-corp.com"
